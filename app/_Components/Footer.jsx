9c8f14b3fff303330fc1cd02d8d42772bee85efe
Nazima Jamalieva <nazimazamalieva836@gmail.com>
2025-05-26 14:24:26 +0600

    feat: add footer mobile layout

--- app/_Components/Footer.jsx
+++ app/_Components/Footer.jsx
@@ -1,21 +1,27 @@
 import { Facebook, Phone, Twitter, Youtube, Mail, MapPin } from "lucide-react";
-
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200/40  text-sm">
-      <div className="container mx-auto px-4 py-28">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-1">
-          <div className="space-y-6 col-span-2">
-            <Image src="/wallet-logo.png" alt="Logo" width={200} height={80} />
-            <p className="text-gray-600 text-lg md:w-4/5">
+    <footer className="bg-gray-200/40 text-sm">
+      <div className="container mx-auto px-4 py-10">
+        <div className="grid grid-cols-1 gap-10 lg:grid-cols-6">
+          {/* Логотип и описание - занимает 2 колонки на больших экранах */}
+          <div className="space-y-6 lg:col-span-2 text-center lg:text-left">
+            <Image
+              src="/wallet-logo.png"
+              alt="Logo"
+              width={200}
+              height={80}
+              className="mx-auto lg:mx-0"
+            />
+            <p className="text-gray-600 text-lg mx-auto lg:mx-0 lg:w-4/5">
               Empowering your financial journey with secure and innovative
               digital wallet solutions.
             </p>
-            <div className="flex space-x-4">
+            <div className="flex justify-center lg:justify-start space-x-4">
               <Facebook
                 className="text-primary hover:text-secondary cursor-pointer transition-colors"
                 size={30}
@@ -30,8 +36,8 @@ const Footer = () => {
               />
             </div>
           </div>
-
-          <div className="space-y-4">
+          {/* Quick Links */}
+          <div className="space-y-4 text-left">
             <h1 className="text-2xl font-semibold text-gray-900 pb-4">
               Quick Links
             </h1>
@@ -48,7 +54,8 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="space-y-4">
+          {/* Services */}
+          <div className="space-y-4 text-left">
             <h2 className="text-xl font-semibold text-gray-900 pb-4">
               Services
             </h2>
@@ -67,7 +74,8 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="space-y-4">
+          {/* Resources */}
+          <div className="space-y-4 text-left">
             <h2 className="text-xl font-semibold text-gray-900 pb-4">
               Resources
             </h2>
@@ -99,10 +107,9 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="space-y-4">
-            <h2 className="text-xl font-semibold text-gray-900 pb-4">
-              Contact Us
-            </h2>
+          {/* Contact Us */}
+          <div className="space-y-4 text-left">
+            <h2 className="text-xl font-semibold text-gray-900 ">Contact Us</h2>
             <div className="space-y-5">
               <div className="flex items-center space-x-3">
                 <Phone className="text-primary" size={20} />
@@ -110,7 +117,6 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="text-primary" size={20} />
-
                 <span className="text-gray-600">support@wallet.com</span>
               </div>
               <div className="flex items-center space-x-3">
@@ -123,8 +129,9 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
       {/* COPYRIGHT */}
-      <div className=" py-8 border-t border-gray-200">
+      <div className="py-8 border-t border-gray-200">
         <p className="text-center text-gray-600">
           © {new Date().getFullYear()} Wallet. All rights reserved.
         </p>
