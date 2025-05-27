31f5d99645112bf7366eb26a4ffbaec575692f41
Nazima Jamalieva <nazimazamalieva836@gmail.com>
2025-05-27 15:42:45 +0600

    feat: add animation on creative section

--- app/_Components/Creative.jsx
+++ app/_Components/Creative.jsx
@@ -7,15 +7,31 @@ const Creative = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-40 items-center mt-10 md:mt-30 px-4 md:px-10 pb-27">
       {/* Блок 1: Текст → Картинка */}
       <div className="space-y-6 md:space-y-10 md:pe-20 text-center md:text-left order-1">
-        <h1 className="text-3xl md:text-5xl font-semibold leading-tight">
+        <h1
+          className="text-3xl md:text-5xl font-semibold leading-tight"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out-back"
+        >
           Creative Design
         </h1>
-        <p className="text-base md:text-xl text-gray-600">
+        <p
+          className="text-base md:text-xl text-gray-600"
+          data-aos="fade-right"
+          data-aos-duration="1200"
+          data-aos-easing="ease-in-out-back"
+        >
           We create stunning and effective designs that capture your brand's
           essence and engage your audience.
         </p>
         <div className="flex justify-center md:justify-start">
-          <Button>Explore Collections</Button>
+          <Button
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-out-back"
+          >
+            Explore Collections
+          </Button>
         </div>
       </div>
       <div className="order-2">
@@ -25,20 +41,39 @@ const Creative = () => {
           width={500}
           height={500}
           className="rounded-lg w-full h-auto object-cover"
+          data-aos="zoom-in-left"
+          data-aos-duration="1200"
+          data-aos-easing="ease-in-out-back"
         />
       </div>
 
       <div className="space-y-6 md:space-y-10 md:pe-20 text-center md:text-left order-3">
-        <h1 className="text-3xl md:text-5xl font-semibold leading-tight">
+        <h1
+          className="text-3xl md:text-5xl font-semibold leading-tight"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out-back"
+        >
           Elevate Your Digital Presence
         </h1>
-        <p className="text-base md:text-xl text-gray-600">
+        <p
+          className="text-base md:text-xl text-gray-600"
+          data-aos="fade-right"
+          data-aos-duration="1200"
+          data-aos-easing="ease-in-out-back"
+        >
           Our cutting-edge solutions help you stand out in the digital
           landscape. From stunning visuals to seamless user experiences, we
           bring brand to life in ways that captivate and convert.
         </p>
         <div className="flex justify-center md:justify-start">
-          <Button>View Our Portfolio</Button>
+          <Button
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-out-back"
+          >
+            View Our Portfolio
+          </Button>
         </div>
       </div>
       <div className="order-4 ">
@@ -48,6 +83,9 @@ const Creative = () => {
           width={500}
           height={500}
           className="rounded-lg w-full h-auto object-cover"
+          data-aos="zoom-in-left"
+          data-aos-duration="1200"
+          data-aos-easing="ease-in-out-back"
         />
       </div>
     </div>
